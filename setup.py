e0dbf5eb340dd322f1ee2e69bc5c3129d740f9a1
david bethke <david.bethke@gmail.com>
2019-01-15 12:21:18 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name='dumbest', version='0.1', description='funny', url='http://github.com/davidbethke/dumbest', author='jk', author_email='jk@gmail.com', license='MIT', packages=['dumbest'], zip_safe=False)
+
